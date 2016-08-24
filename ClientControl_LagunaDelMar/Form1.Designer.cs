805a7e41efd9e70aea249e5c147c6436eab8e0ab
Gibran Amparan <GibranAmparanD@hotmail.com>
2016-08-24 13:09:19 -0700

    Initial Commit

---
+++ ClientControl_LagunaDelMar/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ClientControl_LagunaDelMar
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRequest = new System.Windows.Forms.Button();
+            this.lblGrados = new System.Windows.Forms.Label();
+            this.tbGrados = new System.Windows.Forms.TextBox();
+            this.tbGPM = new System.Windows.Forms.TextBox();
+            this.lblGPM = new System.Windows.Forms.Label();
+            this.tbGPMM = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRequest
+            // 
+            this.btnRequest.Location = new System.Drawing.Point(12, 83);
+            this.btnRequest.Name = "btnRequest";
+            this.btnRequest.Size = new System.Drawing.Size(75, 23);
+            this.btnRequest.TabIndex = 0;
+            this.btnRequest.Text = "Enviar";
+            this.btnRequest.UseVisualStyleBackColor = true;
+            this.btnRequest.Click += new System.EventHandler(this.btnRequest_Click);
+            // 
+            // lblGrados
+            // 
+            this.lblGrados.AutoSize = true;
+            this.lblGrados.Location = new System.Drawing.Point(9, 34);
+            this.lblGrados.Name = "lblGrados";
+            this.lblGrados.Size = new System.Drawing.Size(47, 13);
+            this.lblGrados.TabIndex = 1;
+            this.lblGrados.Text = "Grados: ";
+            // 
+            // tbGrados
+            // 
+            this.tbGrados.Location = new System.Drawing.Point(62, 31);
+            this.tbGrados.Name = "tbGrados";
+            this.tbGrados.ReadOnly = true;
+            this.tbGrados.Size = new System.Drawing.Size(266, 20);
+            this.tbGrados.TabIndex = 2;
+            // 
+            // tbGPM
+            // 
+            this.tbGPM.Location = new System.Drawing.Point(62, 6);
+            this.tbGPM.Name = "tbGPM";
+            this.tbGPM.Size = new System.Drawing.Size(266, 20);
+            this.tbGPM.TabIndex = 4;
+            // 
+            // lblGPM
+            // 
+            this.lblGPM.AutoSize = true;
+            this.lblGPM.Location = new System.Drawing.Point(9, 9);
+            this.lblGPM.Name = "lblGPM";
+            this.lblGPM.Size = new System.Drawing.Size(34, 13);
+            this.lblGPM.TabIndex = 3;
+            this.lblGPM.Text = "GPM:";
+            // 
+            // tbGPMM
+            // 
+            this.tbGPMM.Location = new System.Drawing.Point(62, 57);
+            this.tbGPMM.Name = "tbGPMM";
+            this.tbGPMM.ReadOnly = true;
+            this.tbGPMM.Size = new System.Drawing.Size(266, 20);
+            this.tbGPMM.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "GPMM: ";
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(93, 83);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 0;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 115);
+            this.Controls.Add(this.tbGPMM);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbGPM);
+            this.Controls.Add(this.lblGPM);
+            this.Controls.Add(this.tbGrados);
+            this.Controls.Add(this.lblGrados);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnRequest);
+            this.Name = "frmMain";
+            this.Text = "Control de Valvula de Aceite";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRequest;
+        private System.Windows.Forms.Label lblGrados;
+        private System.Windows.Forms.TextBox tbGrados;
+        private System.Windows.Forms.TextBox tbGPM;
+        private System.Windows.Forms.Label lblGPM;
+        private System.Windows.Forms.TextBox tbGPMM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnActualizar;
+    }
+}
+
---
+++ ClientControl_LagunaDelMar/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace ClientControl_LagunaDelMar
+{
+    public partial class frmMain : Form
+    {
+        String serverIP = @"http://169.254.204.40/";
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRequest_Click(object sender, EventArgs e)
+        {
+            String gpm = tbGPM.Text;
+            ValveParameters valveData = moverValvula(gpm);
+            tbGrados.Text = valveData.servoDegrees;
+            tbGPMM.Text = valveData.GPM;
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            ValveParameters valveData = tomarDatosActuales();
+            tbGrados.Text = valveData.servoDegrees;
+            tbGPMM.Text = valveData.GPM;
+        }
+
+        //Se reportan datos sin enviar nuevos
+        public ValveParameters tomarDatosActuales()
+        {
+            String urlParams = "reportar?";
+            return getValveData(urlParams);
+        }
+
+        //Se envia al servidor el GPM deseado
+        public ValveParameters moverValvula(String GPM)
+        {
+            String urlParams = "mover?GPM=" + GPM;
+            return getValveData(urlParams);
+        }
+
+        private ValveParameters getValveData(String urlParams)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(serverIP + urlParams);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string tempString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            ValveParameters valveData = JsonConvert.DeserializeObject<ValveParameters>(tempString);
+
+            return valveData;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+    public class ValveParameters
+    {
+        public String GPM { get; set; }
+        public String servoDegrees { get; set; }
+    }
+
+}
